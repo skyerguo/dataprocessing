06e6a2d69fe99a0b43cbc41c36767f7a3f5dcad6
skyerguo <skyerguo97@gmail.com>
2018-11-26 21:03:35 +0100

    convert data

---
+++ homework/Week_4/convertCSV2JSON.py
@@ -0,0 +1,38 @@
+# -*- coding:utf8 -*-
+# @author: Tiancheng Guo
+# @contact: skyerguo97@gmail.com
+# @file: convertCSV2JSON.py
+# @time: 11/21/18
+# @description:
+
+import csv
+import json
+
+
+def read_csv(file):
+    # read csv file
+
+    csv_rows = []
+    with open(file) as csvfile:
+        reader = csv.DictReader(csvfile)
+        # get the data from reader
+
+        title = reader.fieldnames
+
+        for row in reader:
+            csv_rows.extend([{title[i]: row[title[i]] for i in range(len(title))}])
+            # extract each line and save in the list 'csv_rows'
+
+    return csv_rows
+
+
+def write_json(data, json_file):
+    # write to json file
+
+    with open(json_file, "w") as f:
+        f.write(json.dumps(data, sort_keys=False, indent=4, separators=(',', ': ')))
+        # save json as a readable file
+
+
+if __name__ == '__main__':
+    write_json(read_csv('data.csv'), 'data.json')
