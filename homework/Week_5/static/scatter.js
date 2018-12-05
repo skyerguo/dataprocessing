3c70ee4899b024a9720e6a0c4e98b2d28dd96c40
skyerguo <skyerguo97@gmail.com>
2018-12-05 22:52:07 +0100

    index.html

--- homework/Week_5/static/scatter.js
+++ homework/Week_5/static/scatter.js
@@ -10,37 +10,52 @@ window.onload = function() {
            the second parameter enable both response possess a country name.
         */
         var dataset = [transformResponse(response[0], 0), transformResponse(response[1], 1)];
-        console.log(dataset);
+        var colorList = ["rgb(255,255,178)", "rgb(254,217,118)", "rgb(254,178,76)", 
+                             "rgb(253,141,60)", "rgb(240,59,32)", "rgb(189,0,38)"]; // 6 blind friendly colors list
+        var countryList = ["France", "Germany", "Korea", "Netherlands", "Portugal", "United Kingdom"]; // 6 countries list
         
-        var width = 800; // visual area width of svg
-        var height = 800; // visual area height of svg
+        var width = 1600; // visual area width of svg
+        var height = 900; // visual area height of svg
 
-        var xAxisWidth = width - 150; // width of x-axis
+        var xAxisWidth = width - 500; // width of x-axis
         var yAxisWidth = height - 150; // height of y-axis
 
         var padding = {top: 20, right: 20, bottom:20, left:50}; // margin of canvas
 
-        var bufferAxis = 30;
+        var bufferAxis = 30; // the gap for Axis
+        var circleRadius = 7;
+
+        var svg, xScale, yScale;
+
+        function initialFigure(Inputdata, datasetNum) {
     
-        /* initialize the svg */
-        var svg = d3.select("body")
-                    .append("svg")
-                    .attr("width", width)
-                    .attr("height", height);
+            /* initialize the svg */
+            svg = d3.select("body")
+                        .append("svg")
+                        .attr("width", width)
+                        .attr("height", height);
 
-        /* x-axis scale (ordinal scale) */
-        var xScale = d3.scaleLinear()
-                        .domain([d3.min(dataset[0], function(d) { return d.time; }),
-                                 d3.max(dataset[0], function(d) { return d.time; })])
-                        .range([bufferAxis, xAxisWidth]);
+            /* x-axis scale (ordinal scale) */
+            xScale = d3.scaleLinear()
+                            .domain([d3.min(Inputdata, function(d) { return d.time; }),
+                                    d3.max(Inputdata, function(d) { return d.time; })])
+                            .range([bufferAxis, xAxisWidth]);
 
-        /* y-axis scale (ordinal scale) */
-        var yScale = d3.scaleLinear()
-                        .domain([d3.min(dataset[0], function(d) { return d.datapoint; }),
-                                 d3.max(dataset[0], function(d) { return d.datapoint; })])
-                        .rangeRound([bufferAxis, yAxisWidth]);
+            /* y-axis scale (ordinal scale) */
+            yScale = d3.scaleLinear()
+                            .domain([d3.min(Inputdata, function(d) { return d.datapoint; }),
+                                    d3.max(Inputdata, function(d) { return d.datapoint; })])
+                            .rangeRound([bufferAxis, yAxisWidth]);
+            
+            /* title of the scatter chart, on the middle and top of total figure */
+            svg.append("text")
+                .attr("class", "title")
+                .attr("x", width / 2 + padding.left)
+                .attr("y", padding.top * 2)
+                .attr("text-anchor", "middle")
+                .text("Scatter Plot")
+        }
 
-        
         function drawScale() {
 
             var xAxis = d3.axisBottom(xScale);
@@ -76,11 +91,10 @@ window.onload = function() {
             yScale.range([bufferAxis, yAxisWidth]);
         }
 
-        function drawCircle() {
-            var colorList = ["red", "orange", "yellow", "green", "cyan", "blue", "purple"]; // 7 regular colors list
-            var countryList = ["France", "Germany", "Korea", "Netherlands", "Portugal", "United Kingdom"]; // 6 countries list
+        function drawCircle(Inputdata) {
 
-            var Circle = svg.selectAll("circle").data(dataset[0])
+            var Circle = svg.selectAll("circle")
+                            .data(Inputdata)
                             .enter()
                             .append("circle")
                             .attr("cx", function(d) {
@@ -89,7 +103,7 @@ window.onload = function() {
                             .attr("cy", function(d) {
                                 return height - padding.bottom - bufferAxis - yScale(d.datapoint)
                             })
-                            .attr("r", 5)
+                            .attr("r", circleRadius)
                             .attr("fill", function(d) {
                                 for (var i = 0; i < countryList.length; i++) {
                                     if (countryList[i] == d.Country) {
@@ -100,8 +114,48 @@ window.onload = function() {
                             });
         }
 
+        function addLegend() {
+
+            var bufferLegend = 30;
+        
+            var legend = svg.selectAll(".legend")
+                            .data(countryList)
+                            .enter()
+                            .append("g")
+                            .attr("class", "legend")
+                            .attr("transform", function (d, i) { return "translate(0," + i * bufferLegend + ")"; });
+                
+            // show the colors of circles in right top as legends symbols
+            legend.append("circle")
+                    .attr("cx", function (d, i) {
+                          return xAxisWidth + 200;
+                    })
+                    .attr("cy", function (d, i) {
+                        return padding.top + bufferAxis + i * bufferLegend;
+                    })
+                    .attr("r", function(d) { return circleRadius; })
+                    .data(countryList)
+                    .style("fill", function(d, i) { 
+                        return colorList[i];
+                    });
+            
+              
+            // add text for each colorful circle about which country it belongs to
+            legend.append("text")
+                    .attr("x", xAxisWidth + 220)
+                    .attr("y", function (d,i) {
+                        return padding.top + bufferAxis + i * bufferLegend;
+                    })
+                    .data(countryList)
+                    .attr("dy", ".35em")
+                    .style("text-anchor", "left")
+                    .text(function (d) { return d});
+        };  
+        
+        initialFigure(dataset[0], 0);
         drawScale();
-        drawCircle();
+        drawCircle(dataset[0]);
+        addLegend()
 
     })
     .catch(function(e) { throw(e); }); // once some errors occur in promise;
